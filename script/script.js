a341083ace3951a6b8fe7c8362e5d3f44b15db81
Shivansh Upadhyay <94211564+goldbrick00@users.noreply.github.com>
2022-03-14 18:17:41 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,40 @@
+let Scrollbar = window.Scrollbar;
+const options = {
+    "damping":.04,
+    "renderByPixels":true,
+    "continousScrolling":true,
+}
+Scrollbar.init(document.querySelector('body'),options);
+
+
+// ANIMATIONS
+TweenMax.from('.text_line_1',1.5,{
+    delay:.5,
+    left:'-100%',
+    opacity:0,
+});
+TweenMax.from('.text_line_2',1.5,{
+    delay:1.5,
+    left:'-100%',
+    opacity:0,
+});
+TweenMax.from('.text_line_3',2.2,{
+    delay:2.4,
+    left:'-100%',
+    opacity:0,
+});
+TweenMax.from('.row-2',2,{
+    delay:4.5,
+    top:'-10%',
+    opacity:0,
+});
+TweenMax.from('.contact_me',2,{
+    delay:2.5,
+    rotation:"-180deg",
+    opacity:0,
+});
+TweenMax.from('.scroll_down_elements',2,{
+    delay:4.5,
+    top:'-10%',
+    opacity:0,
+});
